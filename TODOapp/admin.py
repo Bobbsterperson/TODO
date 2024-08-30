21bc3e4cf44bd8376e0b899d1feb7f1bac650bea
bugston <bugstonn@gmail.com>
2024-08-30 12:48:33 +0300

    added option done to actions dropdown in to do app model

--- TODOapp/admin.py
+++ TODOapp/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
-from .models import TODO, DONE
+from .models import TODO
 
-admin.site.register(TODO)
-admin.site.register(DONE)
+@admin.action(description='DONE')
+def mark_as_completed(modeladmin, request, queryset):
+    for todo in queryset:
+        todo.completed = True
+        todo.save()
+
+@admin.action(description='DELETE')
+def delete_selected(modeladmin, request, queryset):
+    queryset.delete()
+
+class TODOAdmin(admin.ModelAdmin):
+    list_display = ('TODO',)
+    actions = [mark_as_completed, delete_selected]
+
+admin.site.register(TODO, TODOAdmin)
--- TODOapp/models.py
+++ TODOapp/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 
+
 class TODO(models.Model):
     TODO = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.TODO
+    
+
 class DONE(models.Model):
-    DONE = models.BooleanField(default=False)
+    DONE = models.BooleanField(default=True)
 
 
