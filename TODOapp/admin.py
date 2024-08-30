ee1eb5d42b562420b27dc2a2dd6cb1d172b20d75
bugston <bugstonn@gmail.com>
2024-08-30 13:03:50 +0300

    finished now are added to done model

--- TODOapp/admin.py
+++ TODOapp/admin.py
@@ -1,18 +1,26 @@
 from django.contrib import admin
-from .models import TODO
+from .models import TODO, Done
 
 @admin.action(description='DONE')
-def mark_as_completed(modeladmin, request, queryset):
+def mark_as_done(modeladmin, request, queryset):
     for todo in queryset:
-        todo.completed = True
-        todo.save()
+        Done.objects.create(original_todo=todo.TODO, deleted=True)
+        todo.delete()
 
-@admin.action(description='DELETE')
+@admin.action(description='Delete Selected')
 def delete_selected(modeladmin, request, queryset):
-    queryset.delete()
+    for todo in queryset:
+        Done.objects.create(original_todo=todo.TODO, deleted=True)
+        todo.delete()
 
 class TODOAdmin(admin.ModelAdmin):
     list_display = ('TODO',)
-    actions = [mark_as_completed, delete_selected]
+    actions = [mark_as_done, delete_selected]
 
 admin.site.register(TODO, TODOAdmin)
+
+class DoneTODOAdmin(admin.ModelAdmin):
+    list_display = ('original_todo', 'deleted', 'timestamp')
+    readonly_fields = ('timestamp',)
+
+admin.site.register(Done, DoneTODOAdmin)
--- TODOapp/models.py
+++ TODOapp/models.py
@@ -1,14 +1,17 @@
 from django.db import models
 
-
 class TODO(models.Model):
     TODO = models.CharField(max_length=200)
+    # completed = models.BooleanField(default=False)
 
     def __str__(self):
         return self.TODO
-    
-
-class DONE(models.Model):
-    DONE = models.BooleanField(default=True)
 
 
+class Done(models.Model):
+    original_todo = models.CharField(max_length=200)
+    deleted = models.BooleanField(default=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.original_todo} - {"Deleted" if self.deleted else "Completed"}'
